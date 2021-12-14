0acc098dc71c2f39cb71c04ee36732ad84429838
Saransh Mundepi <saransh.mundepi@gmail.com>
2021-12-14 13:01:00 +0530

    Update LongestIncSubSeq.cpp

--- Recursion/LongestIncSubSeq.cpp
+++ Recursion/LongestIncSubSeq.cpp
@@ -3,22 +3,22 @@
 #define MAX(x,y) ((x)>(y)?(x):(y))
 
 
-int longestIncSubSeq(int *a,int m)
+int longestIncSubSeq(int *a,int i,int m)
 {
 	if(m==0)
 		return 0;
 	if(m==1)
 		return 1;
-	if(a[0]<=a[1])
-		return longestIncSubSeq(a+1,m-1)+1;
-	return MAX(longestIncSubSeq(a+1,m-1),longestIncSubSeq(a+2,m-2));
+	if(a[i-1]<=a[i])
+		return longestIncSubSeq(a,i-1,m-1)+1;
+	return MAX(longestIncSubSeq(a,i-1,m-1),longestIncSubSeq(a,i-2,m-2));
 }
 
 int main()
 {
 	int a[]={1,2,3,3,2,1};
 	int m=sizeof(a)/sizeof(int);
-	printf("%d",longestIncSubSeq(a,m));
+	printf("%d",longestIncSubSeq(a,m-1,m));
 	
 	return 0;
 }
